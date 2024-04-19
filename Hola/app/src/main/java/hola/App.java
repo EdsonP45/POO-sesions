7a90e7a23e2e4ccbf70dc6609e8ecbec561220a3
EdsonP45 <167502453+EdsonP45@users.noreply.github.com>
2024-04-18 23:46:53 -0600

    Add files via upload

---
+++ Hola/app/src/main/java/hola/App.java
@@ -0,0 +1,12 @@
+
+package hola;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World soy Edson!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
