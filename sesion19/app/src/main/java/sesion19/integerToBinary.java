b01395de0858577387fe753779de1b98be075e61
EdsonP45 <167502453+EdsonP45@users.noreply.github.com>
2024-04-18 23:43:05 -0600

    Add files via upload

---
+++ sesion19/app/src/main/java/sesion19/integerToBinary.java
@@ -0,0 +1,14 @@
+package mx.utng.sesion19;
+
+import java.util.Scanner;
+
+public class integerToBinary {
+    public static void main(String[] args) {
+     
+        int numero = 40 ;
+        String binario = Integer.toBinaryString(numero);
+        System.out.printf(" el numero %d es en binario %s%n", numero,  binario);
+
+    }
+    
+}
---
+++ sesion19/app/src/test/java/sesion19/HelloWorld.java
@@ -0,0 +1,8 @@
+package sesion19;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hola mondo desde java");
+    }
+    
+}
